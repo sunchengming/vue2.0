1c3de377722aad5a561437cd7bfa693969eba79e
sunchengming <1164411610@qq.com>
2023-02-17 18:30:32 +0800

    2.0

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <ul>
       <li>
-        <v-link href="/">Home111111</v-link>
+        <v-link href="/">Home</v-link>
         <v-link href="/about">About</v-link>
       </li>
     </ul>
